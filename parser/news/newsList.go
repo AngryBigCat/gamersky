df4ae2ecf6491e95d7487b7037a131a1de1181f7
AngryCat <angrycat123@163.com>
2018-09-28 18:54:42 +0800

    concurrent script

--- parser/news/newsList.go
+++ parser/news/newsList.go
@@ -1,21 +1,24 @@
 package news
 
 import (
-	"regexp"
+	"fmt"
 	"gamersky/engine"
+	"regexp"
 )
 
 const newsListRe = `<li>.*?<a class=\\"dh\\".*?>(.*?)</a>.*?<a class=\\"tt\\" href=\\"(.*?)\\".*?>(.*?)</a>.*?<div class=\\"txt\\">(.*?)</div>.*?<img src=\\"(.*?)\\".*?<div class=\\"time\\">(.*?)</div>.*?</li>`
+
 func ParseNewsList(content []byte) engine.ParserResult {
 	compile := regexp.MustCompile(newsListRe)
 	submatch := compile.FindAllSubmatch(content, -1)
-	result := engine.ParserResult{}
-	for _, v := range submatch {
-		result.Items = append(result.Items, v[3])
-		result.Requests = append(result.Requests, engine.Request{
-			Url: string(v[2]),
+	parserResult := engine.ParserResult{}
+	for k, v := range submatch {
+		fmt.Printf("%d: %s  %s\n", k, v[3], v[2])
+		parserResult.Items = append(parserResult.Items, v[3])
+		parserResult.Requests = append(parserResult.Requests, engine.Request{
+			Url:        string(v[2]),
 			ParserFunc: engine.NilParserFunc,
 		})
 	}
-	return result
+	return parserResult
 }
--- scheduler/simple.go
+++ scheduler/simple.go
@@ -1,13 +1,17 @@
 package scheduler
 
-import "gamersky/engine"
+import (
+	"gamersky/engine"
+)
 
 type SimpleScheduler struct {
 	workerChan chan engine.Request
 }
 
-func (s *SimpleScheduler) Submit(request engine.Request) {
-	s.workerChan <- request
+func (s *SimpleScheduler) Submit(r engine.Request) {
+	go func() {
+		s.workerChan <- r
+	}()
 }
 
 func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
